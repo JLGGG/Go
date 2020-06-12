c17e63968ec3052ff9553977030cad8cf9c7d5ea
Junsu Lim <blockboy2957@gmail.com>
2020-06-12 23:19:27 +0900

    Create constructor_pattern.go

---
+++ src/constructor_pattern.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width  int
+	height int
+}
+
+func Constructor(width, height int) *Rectangle {
+	return &Rectangle{width, height}
+}
+
+func main() {
+	rect := Constructor(20, 10)
+
+	fmt.Println(rect)
+}
